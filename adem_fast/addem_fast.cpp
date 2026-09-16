b373b7867826225a3483d55d577a12288567a311
liz diaz <liz.diaz-lopez@sjsu.edu>
2026-09-15 23:06:07 -0700

    updated the second question

--- adem_fast/addem_fast.cpp
+++ adem_fast/addem_fast.cpp
@@ -13,6 +13,8 @@ int main (int argc, char* argv[]){
     std:: minstd_rand gen(name.length()); //Random Numbers
     auto start  = std::chrono::steady_clock::now(); //Timing
     int seconds = argv[2] ; // Second Argument
+    int correctnumber = 0;
+    bool firstquestion = true;
 
     While (true) {
         int firstnumber;
@@ -39,32 +41,12 @@ int main (int argc, char* argv[]){
             std::cout << totalsum << "." << std::endl;
             break;
         }
-
-
-
-
+     }
+        std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - start;
+        if (elapsed.count() > seconds) {
+            break;
         }
+        std::cout << "You got " << correctnumber << " correct!" << std::endl;
 
-    }
-    std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - start;
-
-
-
-    std::string name = argv[] ;
-    int firstnumber = name.length();
-    int secondnumber = (firstnumber +13)/7;
-    int totalsum = firstnumber + secondnumber; 
-    std::cout << "Hello " << name << " what is ";
-    std::cout << firstnumber << "+" << secondnumber << std::endl;
-    int answer; 
-    std:: cin >> answer;
-    if (answer ==  totalsum){
-        std::cout << "Correct " << name << "!" << std::endl;
-    } else {
-        std::cout << "No " << name << ", it is ";
-        std::cout << totalsum << "." << std ::endl;
-    }
     return 0;
-    std::cout << "No ";
-
 }
