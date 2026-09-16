bf7692eb23f22ac760708e4d573ae72a20f38544
liz diaz <liz.diaz-lopez@sjsu.edu>
2026-09-16 00:05:59 -0700

    Fixed small mistakes

--- adem_fast/addem_fast.cpp
+++ adem_fast/addem_fast.cpp
@@ -12,25 +12,25 @@ int main (int argc, char* argv[]){
     std::string name = argv[1] ; //The Game | First Argument
     std:: minstd_rand gen(name.length()); //Random Numbers
     auto start  = std::chrono::steady_clock::now(); //Timing
-    int seconds = argv[2] ; // Second Argument
+    int seconds = std::stoi(argv[2]) ; // Second Argument
     int correctnumber = 0;
     bool firstquestion = true;
 
-    While(true) {
+    while(true) {
         int firstnumber;
         int secondnumber;
-        int totalsum = firstnumber + second number;
-
+       
         if (firstquestion) {
-            int firstnumber = name.length();
-            int secondnumber = (firstnumber +13)%7;
-            int firstquestion = false;
+            firstnumber = name.length();
+            secondnumber = (firstnumber +13) % 7;
+            firstquestion = false;
 
         }else {
             firstnumber = gen() % 100;
             secondnumber = gen() % 100;
         }
-
+        int totalsum = firstnumber + secondnumber;
+        
         std::cout << "Hello " << name << "what is ";
         std::cout << firstnumber << "+" << secondnumber << std::endl;
         
@@ -45,7 +45,7 @@ int main (int argc, char* argv[]){
             std::cout << totalsum << "." << std::endl;
             break;
         }
-        std::cout << "Correct " << name << "!" <<endl;
+        std::cout << "Correct " << name << "!" << std::endl;
         correctnumber++;
         std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - start;
         
